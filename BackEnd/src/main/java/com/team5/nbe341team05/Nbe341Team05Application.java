dbbac74ca15d82c87e78a609ade0f8f76424c9b0
rOyalFruit <ekdeon@gmail.com>
2025-01-14 15:23:31 +0900

    Initial commit

---
+++ BackEnd/src/main/java/com/team5/nbe341team05/Nbe341Team05Application.java
@@ -0,0 +1,13 @@
+package com.team5.nbe341team05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe341Team05Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbe341Team05Application.class, args);
+    }
+}
