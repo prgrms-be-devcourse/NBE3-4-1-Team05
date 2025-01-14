33835f71ad598a34fac5bbba8e32e47ba1ecb679
segye <tprpek@gmail.com>
2025-01-14 18:47:48 +0900

    Feat:주문 Controller 추가

--- BackEnd/src/main/java/com/team5/nbe341team05/domain/order/controller/OrderController.java
+++ BackEnd/src/main/java/com/team5/nbe341team05/domain/order/controller/OrderController.java
@@ -1,4 +1,31 @@
 package com.team5.nbe341team05.domain.order.controller;
 
+import com.team5.nbe341team05.common.response.ResponseMessage;
+import com.team5.nbe341team05.domain.order.dto.OrderDto;
+import com.team5.nbe341team05.domain.order.dto.OrderResponseDto;
+import com.team5.nbe341team05.domain.order.entity.Order;
+import com.team5.nbe341team05.domain.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseMessage<OrderResponseDto> createOrder(@RequestBody OrderDto orderDto) {
+        Order order = orderService.createOrder(orderDto);
+
+        return new ResponseMessage<>(
+                "주문이 성공적으로 생성되었습니다.",
+                String.valueOf(HttpStatus.OK.value()),
+                new OrderResponseDto(order)
+        );
+    }
 }
