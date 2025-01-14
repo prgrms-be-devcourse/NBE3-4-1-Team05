c5296d1b864159c7f59d67ad9e48a339d73b1e93
segye <tprpek@gmail.com>
2025-01-14 19:04:46 +0900

    Feat: 예외처리 추가

---
+++ BackEnd/src/main/java/com/team5/nbe341team05/common/GlobalExceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,80 @@
+package com.team5.nbe341team05.common.GlobalExceptionHandler;
+
+import com.team5.nbe341team05.common.exceptions.ServiceException;
+import com.team5.nbe341team05.common.response.ResponseMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+@RequiredArgsConstructor
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ResponseMessage<Void>> handle(NoHandlerFoundException ex) {
+
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ResponseMessage<>(
+                        "해당 데이터가 존재하지 않습니다.",
+                        "404",
+                        null
+                ));
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ResponseMessage<Void>> handle(NoSuchElementException ex) {
+
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ResponseMessage<>(
+                        "해당 데이터가 존재하지 않습니다."
+                        , "404",
+                        null
+                ));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseMessage<Void>> handle(MethodArgumentNotValidException ex) {
+
+
+        String message = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .filter(error -> error instanceof FieldError)
+                .map(error -> (FieldError) error)
+                .map(error -> error.getField() + "-" + error.getCode() + "-" + error.getDefaultMessage())
+                .sorted(Comparator.comparing(String::toString))
+                .collect(Collectors.joining("\n"));
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ResponseMessage<>(
+                        message
+                        , "400",
+                        null
+                ));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<ResponseMessage<Void>> handle(ServiceException ex) {
+
+        ResponseMessage<Void> rsData = ex.getRsData();
+
+        return ResponseEntity
+                .status(rsData.getStatusCode())
+                .body(rsData);
+    }
+}
--- BackEnd/src/main/java/com/team5/nbe341team05/common/response/ResponseMessage.java
+++ BackEnd/src/main/java/com/team5/nbe341team05/common/response/ResponseMessage.java
@@ -1,9 +1,16 @@
 package com.team5.nbe341team05.common.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+
 
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ResponseMessage<T>(String message, String resultCode, T data) {
+    @JsonIgnore
+    public int getStatusCode() {
+        return Integer.parseInt(resultCode.split("-")[0]);
+    }
 }
