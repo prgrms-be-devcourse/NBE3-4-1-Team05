4cd2c20e1515f7dc1ec7bc766588a577a97708df
kjm722 <kjm722@naver.com>
2025-01-15 12:57:36 +0900

    Fix: Admin 엔티티 오류 수정

--- BackEnd/src/main/java/com/team5/nbe341team05/domain/admin/entity/Admin.java
+++ BackEnd/src/main/java/com/team5/nbe341team05/domain/admin/entity/Admin.java
@@ -3,13 +3,14 @@ package com.team5.nbe341team05.domain.admin.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@Builder
 @RequiredArgsConstructor
 public class Admin {
 
