d7d8ee65e50b6673e181024e725f79254f34018a
kjm722 <kjm722@naver.com>
2025-01-15 13:25:48 +0900

    Feat: admin 기능을 위한 메소드 추가

--- BackEnd/src/main/java/com/team5/nbe341team05/domain/admin/entity/Admin.java
+++ BackEnd/src/main/java/com/team5/nbe341team05/domain/admin/entity/Admin.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Entity
---
+++ BackEnd/src/main/java/com/team5/nbe341team05/domain/order/dto/OrderResponseDto.java
@@ -0,0 +1,34 @@
+package com.team5.nbe341team05.domain.order.dto;
+
+import com.team5.nbe341team05.domain.cartMenu.dto.CartMenuDto;
+import com.team5.nbe341team05.domain.order.entity.Order;
+import com.team5.nbe341team05.domain.orderMenu.Dto.OrderMenuDto;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+public class OrderResponseDto {
+    private String email;
+    private String address;
+    private LocalDateTime order_time;
+    private int totalPrice;
+    private boolean deliveryStatus;
+    private List<OrderMenuDto> omlist;
+
+    public OrderResponseDto(Order order) {
+        this.email = order.getEmail();
+        this.address = order.getAddress();
+        this.order_time = order.getOrderTime();
+        this.totalPrice = order.getTotalPrice();
+        this.deliveryStatus = order.isDeliveryStatus();
+        this.omlist = order.getOrderMenus().stream()
+                .map(orderMenu -> new OrderMenuDto(
+                        orderMenu.getMenu().getProductName(),
+                        orderMenu.getQuantity())).toList();
+    }
+}
