7d1254f92c521a411c0d3d7048e2011514176196
kjm722 <kjm722@naver.com>
2025-01-15 13:41:45 +0900

    FIX: Merge 후 Order 패키지 수정

--- BackEnd/src/main/java/com/team5/nbe341team05/domain/order/controller/OrderController.java
+++ BackEnd/src/main/java/com/team5/nbe341team05/domain/order/controller/OrderController.java
@@ -6,8 +6,12 @@ import com.team5.nbe341team05.domain.order.dto.OrderResponseDto;
 import com.team5.nbe341team05.domain.order.dto.OrderUpdateRequestDto;
 import com.team5.nbe341team05.domain.order.entity.Order;
 import com.team5.nbe341team05.domain.order.service.OrderService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -67,5 +71,29 @@ public class OrderController {
                 String.valueOf(HttpStatus.OK.value()),
                 null
         );
+    }
+
+    @Transactional
+    @DeleteMapping("/admin/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseMessage<OrderResponseDto> cancelOrder(@PathVariable long id) {
+        orderService.cancelAdminOrder(id);
+    return new ResponseMessage<>(
+                "주문이 성공적으로 취소되었습니다.",
+                String.valueOf(HttpStatus.OK.value()),
+                null);
+    }
+
+    @Transactional
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Page<OrderResponseDto>> getOrderListForAdmin( // 추후에 가능하면 이메일 별 검색 기능 추가
+                                                                        @RequestParam(defaultValue = "0") int page, // 0-based index
+                                                                        @RequestParam(defaultValue = "10") int size,
+                                                                        @RequestParam(defaultValue = "id") String sortBy,
+                                                                        @RequestParam(defaultValue = "DESC") String sortDir
+    ) {
+        Page<OrderResponseDto> pagedOrders = orderService.getPagedOrders(page, size, sortBy, sortDir);
+        return ResponseEntity.ok(pagedOrders);
     }
 }
--- BackEnd/src/main/java/com/team5/nbe341team05/domain/order/service/OrderService.java
+++ BackEnd/src/main/java/com/team5/nbe341team05/domain/order/service/OrderService.java
@@ -14,6 +14,10 @@ import com.team5.nbe341team05.domain.order.entity.Order;
 import com.team5.nbe341team05.domain.order.repository.OrderRepository;
 import com.team5.nbe341team05.domain.orderMenu.entity.OrderMenu;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -150,5 +154,20 @@ public class OrderService {
 
     public Optional<Order> findFirst() {
         return orderRepository.findFirstByOrderByIdDesc();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<OrderResponseDto> getPagedOrders(int page, int size, String sortBy, String sortDir) {
+        Sort sort = sortDir.equalsIgnoreCase("ASC") ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<Order> orders = orderRepository.findAll(pageable);
+        return orders.map(OrderResponseDto::new);
+    }
+
+    @Transactional
+    public void cancelAdminOrder(Long id) {
+        Order order = orderRepository.findById(id)
+                .orElseThrow(() -> new ServiceException("404", "해당 주문을 찾을 수 없습니다."));
+        this.orderRepository.delete(order);
     }
 }
