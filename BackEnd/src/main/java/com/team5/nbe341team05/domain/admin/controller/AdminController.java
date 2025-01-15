7472785abc9277d5e8f72b771d797b5fa21b7786
kjm722 <kjm722@naver.com>
2025-01-15 12:38:32 +0900

    Feat: Admin 회원가입

--- BackEnd/src/main/java/com/team5/nbe341team05/domain/admin/controller/AdminController.java
+++ BackEnd/src/main/java/com/team5/nbe341team05/domain/admin/controller/AdminController.java
@@ -1,9 +1,24 @@
 package com.team5.nbe341team05.domain.admin.controller;
 
+import com.team5.nbe341team05.domain.admin.entity.Admin;
+import com.team5.nbe341team05.domain.admin.service.AdminService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/admin")
 @RequiredArgsConstructor
 @RestController
 public class AdminController {
+
+    private final AdminService adminService;
+
+    @PostMapping
+    public Admin joinAdmin(
+            @RequestParam(value = "username") String username,
+            @RequestParam(value = "password") String password) {
+        return this.adminService.join(username, password);
+    }
 }
--- BackEnd/src/main/java/com/team5/nbe341team05/domain/admin/service/AdminService.java
+++ BackEnd/src/main/java/com/team5/nbe341team05/domain/admin/service/AdminService.java
@@ -1,9 +1,22 @@
 package com.team5.nbe341team05.domain.admin.service;
 
+import com.team5.nbe341team05.domain.admin.entity.Admin;
+import com.team5.nbe341team05.domain.admin.repository.AdminRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AdminService {
+
+    private final AdminRepository adminRepository;
+
+    public Admin join(String username, String password) {
+        Admin admin = Admin.builder()
+                .username(username)
+                .password(password)
+                .build();
+
+        return adminRepository.save(admin);
+    }
 }
